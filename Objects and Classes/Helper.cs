a6d3bcb174a9d15e27ef0e6e5517b90f22cdc489
seyidabassa <124425968+seyidabassa@users.noreply.github.com>
2023-03-07 13:52:35 +0400

    A program

--- Objects and Classes/Helper.cs
+++ Objects and Classes/Helper.cs
@@ -8,16 +8,33 @@ namespace Objects_and_Classes
 {
     internal class Helper
     {
-        public string ReadName()
+        static public string ReadString(string question)
         {
         l1:
-            Console.Write($"Student name:");
+            Console.Write(question);
             string name = Console.ReadLine();
             if (string.IsNullOrWhiteSpace(name))
             {
                 goto l1;
             }
             return name;
+
         }
-    }
+        
+        static public int ReadInt(string question)
+        {
+            int value;
+        l2:
+                Console.Write(question);
+                string valueStr = Console.ReadLine();  
+               //student.age = int.Parse(Console.ReadInt()); // use tryparse later and write in your notepad 
+                if (!int.TryParse(valueStr, out value))
+                {
+                Console.WriteLine($"{valueStr} is not correct");
+                    goto l2;
+                }
+            return value;
+        }   
+
+     }
 }
--- Objects and Classes/Program.cs
+++ Objects and Classes/Program.cs
@@ -20,44 +20,21 @@ namespace Objects_and_Classes
             Student[] individual = new Student[numberOfStudents];
             for (int i = 0; i < numberOfStudents; i++)
             {
-                
-                Student student = new Student();
-                
-                l2:
-                Console.Write($"Student number {i + 1} surname:");
-                student.surname = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(student.surname))
-                {
-                    goto l2;
-                }
-
-                l3:
-                Console.Write($"Student number {+1} age:");
-                student.age = int.Parse(Console.ReadLine()); // use tryparse later and write in your notepad 
-                if (student.age==0)
-                {
-                    goto l3;
-                }
-
-                l4:
-                Console.Write($"Student number {i + 1} speciality:");
-                student.speciality = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(student.speciality))
-                {
-                    goto l4;
-                }
-                Console.Write($"Student number {i + 1} Group number:");
-                student.groupNo = int.Parse(Console.ReadLine());
-
-
+                //CLasses are highlighted in Green and Objects in blue
+                Student student = new Student(); // pod klassom Student sozdaem object student
+                //Helper helper = new Helper();//Dayom znakomstvo, reference s Helper, pod classom Helper sozdaem object helper
+                student.name = Helper.ReadString($"Student {i+1} Name:"); //calling our method from Helper class (if static)
+                student.surname = Helper.ReadString($"Student {i+1} Surname:");
+                student.age= Helper.ReadInt($"Student {i+1} age:");
+                student.speciality = Helper.ReadString($"Student {i + 1} Speciality:");
+                student.groupNo = Helper.ReadInt($"Student {i + 1} Group number:");
                 individual[i] = student;
-                ;
-
+                
             }
                 Console.ForegroundColor= ConsoleColor.Green;
                 for (int i2 = 0; i2 < numberOfStudents; i2++)
                 {
-                    Console.WriteLine(individual[i2]);
+                    Console.WriteLine($"{i2+1} {individual[i2]} ");
                 }
             Console.ForegroundColor = ConsoleColor.White;
 
